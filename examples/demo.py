ce30835947c2147198f09660d53261c68e38297e
Matthias Diener <mdiener@illinois.edu>
2024-12-19 11:12:05 +0100

    Update README

--- examples/demo.py
+++ examples/demo.py
@@ -8,9 +8,21 @@ assert cd == {1: 2}
 # constantdicts are hashable, and their hashes are cached
 print(hash(cd), cd)
 
-# Attempting to modify the constantdict raises an AttributeError
+# Attempting to modify a constantdict in-place raises an AttributeError
 try:
-    # Similar for pop(), popitem(), clear(), del, and setdefault()
+    # Similar for pop(), popitem(), clear(), and del
     cd[4] = 12
 except AttributeError:
     pass
+
+# Some methods return a mutated copy of a constantdict
+cd_new = cd.setdefault(10,5)
+assert cd_new == {1: 2, 10: 5}
+# Similar for set(), update(), delete(), discard()
+
+# Performing multiple mutations can be faster via mutate()/finish()
+cd_mut = cd.mutate()  # cd_mut is a mutable copy of cd
+cd_mut[42] = 0
+del cd_mut[1]
+cd_new2 = cd_mut.finish()  # cd_new2 is an immutable version of cd_mut
+assert cd_new2 == {42: 0}
